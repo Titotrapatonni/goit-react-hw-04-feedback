46deb589ec518bb3395cb41e9564a25da0107236
Titotrapatonni <titovtelexfree@gmail.com>
2023-03-13 15:13:32 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,54 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { GlobalStyle } from './GlobalStyle';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addFeedback = key => {
+    switch (key) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  addFeedback = key => {
-    this.setState(prevState => {
-      return { [key]: (prevState[key] += 1) };
-    });
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((100 * good) / countTotalFeedback());
+  };
 
-  countPositiveFeedbackPercentage() {
-    return Math.round((100 * this.state.good) / this.countTotalFeedback());
-  }
+  return (
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={addFeedback}
+      ></FeedbackOptions>
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.addFeedback}
-        ></FeedbackOptions>
-
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          percentage={this.countPositiveFeedbackPercentage()}
-        ></Statistics>
-        <GlobalStyle />
-      </Section>
-    );
-  }
-}
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={countTotalFeedback()}
+        percentage={countPositiveFeedbackPercentage()}
+      ></Statistics>
+      <GlobalStyle />
+    </Section>
+  );
+};
